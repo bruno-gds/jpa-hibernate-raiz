0686278b687d3dbb2dd641b4dd0901e3dd81dc72
bruno-gds <brunog.damascena@gmail.com>
2023-09-04 22:36:49 -0300

    consultas avançadas com JPQL

--- src/main/java/br/com/alura/loja/modelo/Produto.java
+++ src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -12,6 +12,8 @@ import java.time.LocalDate;
 
 @Entity
 @Table(name = "produtos")
+@NamedQuery(name = "Produto.produtosPorCategoria",
+        query = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome")
 public class Produto {
 
     @Id
--- src/main/java/br/com/alura/loja/testes/CadastroDePedido.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDePedido.java
@@ -6,9 +6,11 @@ import br.com.alura.loja.dao.PedidoDao;
 import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.modelo.*;
 import br.com.alura.loja.util.JPAUtil;
+import br.com.alura.loja.vo.RelatorioDeVendasVo;
 
 import javax.persistence.EntityManager;
 import java.math.BigDecimal;
+import java.util.List;
 
 /**
  * @author Bruno Gomes Damascena dos santos (bruno-gds) < brunog.damascena@gmail.com >
@@ -26,26 +28,52 @@ public class CadastroDePedido {
         ClienteDao clienteDao = new ClienteDao(em);
 
         Produto produto = produtoDao.buscarPorId(1L);
+        Produto produto2 = produtoDao.buscarPorId(2L);
+        Produto produto3 = produtoDao.buscarPorId(3L);
         Cliente cliente = clienteDao.buscarPorId(1L);
 
         em.getTransaction().begin();
 
         Pedido pedido = new Pedido(cliente);
         pedido.adicionarItem(new ItemPedido(10, pedido, produto));
+        pedido.adicionarItem(new ItemPedido(40, pedido, produto2));
+
+        Pedido pedido2 = new Pedido(cliente);
+        pedido.adicionarItem(new ItemPedido(2, pedido2, produto3));
 
         PedidoDao pedidoDao = new PedidoDao(em);
         pedidoDao.cadastrar(pedido);
+        pedidoDao.cadastrar(pedido2);
 
         em.getTransaction().commit();
+
+        BigDecimal valorTotalVendido = pedidoDao.valorTotalVendido();
+        System.out.println("Valor total vendido: " + valorTotalVendido);
+
+        List<RelatorioDeVendasVo> relatorio = pedidoDao.relatorioDeVendas();
+        relatorio.forEach(System.out::println);
     }
 
     private static void popularBandoDeDados() {
-        Categoria celuares = new Categoria("Celulares");
+        Categoria celuares = new Categoria("CELUARES");
+        Categoria videogames = new Categoria("VIDEOGAMES");
+        Categoria informatica = new Categoria("INFORMATICA");
+
         Produto celular = new Produto(
                 "Xiomi Redmi",
                 "Muito legal",
                 new BigDecimal("500"),
                 celuares);
+        Produto videogame = new Produto(
+                "PS5",
+                "Playstation 5",
+                new BigDecimal("500"),
+                videogames);
+        Produto computador = new Produto(
+                "Macbook",
+                "Macbook Pro",
+                new BigDecimal("500"),
+                informatica);
 
         Cliente cliente = new Cliente("Bruno", "123.456.789-00");
 
@@ -57,7 +85,13 @@ public class CadastroDePedido {
         em.getTransaction().begin();
 
         categoriaDao.cadastrar(celuares);
+        categoriaDao.cadastrar(videogames);
+        categoriaDao.cadastrar(informatica);
+
         produtoDao.cadastrar(celular);
+        produtoDao.cadastrar(videogame);
+        produtoDao.cadastrar(computador);
+
         clienteDao.cadastrar(cliente);
 
         em.getTransaction().commit();
